d9bd4478b23f3a8333c0cf43bf8b9e97e814a337
Rachel Kim <rachelkim8084@gmail.com>
2020-12-02 21:31:36 -0800

    this sucks.

--- src/main/java/edu/uwb/css143b2020fall/service/IndexerImpl.java
+++ src/main/java/edu/uwb/css143b2020fall/service/IndexerImpl.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.Map;
 
 @Service
-public class IndexerImpl implements Indexer {
+public class IndexerImpl implements Indexer { //doc num 2 index 2
     public Map<String, List<List<Integer>>> index(List<String> docs) {
         Map<String, List<List<Integer>>> indexes = new HashMap<>();
         int docNum = 0;
@@ -18,15 +18,26 @@ public class IndexerImpl implements Indexer {
             for (String word: words) {
                 if (!indexes.containsKey(word)) {
                     List<List<Integer>> value = new ArrayList<>();
-                    List<Integer> atIndex = new ArrayList<>();
-                    value.add(docNum, atIndex);
+                    for (int i = 0; i <= docNum; i++) {
+                        List<Integer> atIndex = new ArrayList<>();
+                        value.add(atIndex);
+                    }
                     indexes.put(word, value);
                 }
                 List<List<Integer>> listOfDocs = indexes.get(word);
-                List<Integer> listOfInd = listOfDocs.get(docNum);
+                if (listOfDocs.size() <= docNum) { //add another array for the next doc, otherwise out of bounds error
+                    for (int j = listOfDocs.size(); j <= docNum; j++) {
+                        List<Integer> atIndex = new ArrayList<>();
+                        listOfDocs.add(atIndex);
+                    }
+                    List<Integer> add = new ArrayList<>();
+                    listOfDocs.add(docNum, add);
+                }
+                List<Integer> listOfInd = listOfDocs.get(docNum); //index 1 out of bounds for length 1
                 listOfInd.add(wordIndex);
+                listOfDocs.remove(docNum); //maybe remove. address is shared
                 listOfDocs.add(docNum, listOfInd);
-                indexes.put(word, listOfDocs);
+                indexes.replace(word, listOfDocs); //this is a problem,
                 wordIndex++;
             }
             docNum++;
